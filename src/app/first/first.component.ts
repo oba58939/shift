af8c4dc8f83958c1c2fae18fc6adf4613b67e34d
oba58939 <ohbahinako@gmail.com>
2024-12-12 16:10:10 +0900

    fix: change scss to css

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-first',
   templateUrl: './first.component.html',
-  styleUrls: ['./first.component.scss']
+  styleUrl: './first.component.css'
 })　
-export class FirstComponent implements OnInit{
+export class FirstComponent {
   stores = ['店舗A','店舗B','店舗C'];
   staffShift: StaffShift = {
     store: '',
